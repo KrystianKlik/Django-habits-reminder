6dabc4f5d8cfc3062f771a4f6b3d731989a9c17c
Krystian Klik <krystianklik@gmail.com>
2020-02-21 17:35:57 +0100

    Initial commit

---
+++ Django-habits-reminder/Django-habits-reminder/Django_habits_reminder/__init__.py
@@ -0,0 +1,3 @@
+"""
+Package for Django_habits_reminder.
+"""
---
+++ Django-habits-reminder/Django-habits-reminder/django-habits/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\kryst\Documents\Django-habits-reminder\Django-habits-reminder\Django-habits-reminder\django-habits\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
