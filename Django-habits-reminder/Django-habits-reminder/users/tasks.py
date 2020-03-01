f5b5ef59b0801adf067e8b8cde433281d6c06e85
Krystian Klik <krystianklik@gmail.com>
2020-03-01 13:40:31 +0100

    Clean tasks.py

--- Django-habits-reminder/Django-habits-reminder/users/tasks.py
+++ Django-habits-reminder/Django-habits-reminder/users/tasks.py
@@ -6,15 +6,15 @@ from .models import Profile
 from django.contrib.auth.models import User
 from celery.contrib import rdb
 
-@task(name='summary') 
-#@periodic_task(run_every=crontab(minute="*", hour=0)) # It will run your task at midnight
+@task(name='Set Current Strike To Zero') 
 def SetCurrentStrikeToZero():
 
+    # This code is for testing, it shoukld reset current_strike_count to 0
     users = User.objects.all()
     #If user didn't done all habits his current strike equal 0
     for user in users:
         user.profile.current_strike_count = 0
-        rdb.set_trace()
+
         
 
     
