07102ad9805be4d9d757db20d0a4c29746026f99
Krystian Klik <krystianklik@gmail.com>
2020-03-01 13:13:11 +0100

    Test if user is created and is able to log in

--- Django-habits-reminder/Django-habits-reminder/habits/tests.py
+++ Django-habits-reminder/Django-habits-reminder/habits/tests.py
@@ -1,26 +1,27 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 import django
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+from django.urls import reverse, resolve
+from .views import HabitsListView, index, HabitsListSetup, HabitsCreateView, HabitsUpdateView, HabitsDeleteView
+from django.contrib.auth.models import User
 
-# TODO: Configure your database in settings.py and sync before running tests.
+class TestUrls(SimpleTestCase):
 
-class SimpleTest(TestCase):
-    """Tests for the application views."""
+    def test_list_of_urls(self):
+        index = self.client.get('/')
+        self.assertEqual(index.status_code, 200)
 
-    # Django requires an explicit setup() when running tests in PTVS
-    @classmethod
-    def setUpClass(cls):
-        super(SimpleTest, cls).setUpClass()
-        django.setup()
 
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class LogInTest(TestCase):
+    def setUp(self):
+        self.credentials = {
+            'username': 'testuser',
+            'password': 'secret'}
+        User.objects.create_user(**self.credentials)
+    def test_login(self):
+        # send login data
+        response = self.client.post('/login/', self.credentials, follow=True)
+        # should be logged in now
+        self.assertTrue(response.context['user'].is_authenticated)
+
+
+
