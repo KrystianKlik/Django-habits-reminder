b64e421f478adf4116eb795cd473520c55d3ceb4
Krystian Klik <krystianklik@gmail.com>
2020-03-01 03:06:24 +0100

    Responsive habits list

--- Django-habits-reminder/Django-habits-reminder/users/tasks.py
+++ Django-habits-reminder/Django-habits-reminder/users/tasks.py
@@ -1,21 +1,20 @@
-from celery import shared_task
+from celery import shared_task, task
 from time import sleep
 from celery.task import periodic_task 
 from celery.schedules import crontab
 from .models import Profile
 from django.contrib.auth.models import User
+from celery.contrib import rdb
 
-@periodic_task(run_every=crontab(minute=0, hour=0)) # It will run your task at midnight
-def schedule_task():
+@task(name='summary') 
+#@periodic_task(run_every=crontab(minute="*", hour=0)) # It will run your task at midnight
+def SetCurrentStrikeToZero():
 
     users = User.objects.all()
     #If user didn't done all habits his current strike equal 0
     for user in users:
-        if user.habits.status == False:
-            user.profile.current_strike_count = 0
-        #else:
-            # If user did done all habits then it adds +1 to current strike plus checks if current strike is bigger then longest recored strike
-            #user.profile.current_strike_count =  user.profile.current_strike_count + 1
-            #if user.profile.longest_strike > user.profile.current_strike_count:
-            #    user.profile.longest_strike = user.profile.current_strike_count
+        user.profile.current_strike_count = 0
+        rdb.set_trace()
+        
+
     
